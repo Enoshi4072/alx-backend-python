76b7224de51bb336e344cd3e4927fabe892d4f99
Enoshi4072 <enockkibet6642@gmail.com>
2024-03-15 05:44:14 +0000

    11. More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,9 +2,10 @@
 """ Using Typevar, mapping, Any, Union """
 from typing import TypeVar, Mapping, Any, Union
 T = TypeVar('T')
+opt = Union[T, None]
 
 
-def safely_get_value(dct: Mapping, key: Any, default: T = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: opt = None) -> Union[Any, T]:
 
     """ Implementing the function """
     if key in dct:
